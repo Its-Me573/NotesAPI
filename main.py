840935c4b380832e608659a8ad43d72bfb2139d6
armando <231827038+Its-Me573@users.noreply.github.com>
2026-07-27 22:08:19 -0700

    feat: add Pydantic model for fetch with body in JavaScript
    chore: add change_date_modified helper function

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import helper
 
 from fastapi.middleware.cors import CORSMiddleware
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 app.add_middleware(
@@ -21,10 +23,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#For initial note creation
+class Creation_Note(BaseModel):
+    name: str
+    content: str | None = None
+    date_created: str
+    date_modified: str
+
+
+#For Name Modification
+class Name_Modification_Note(BaseModel):
+    new_name: str
+    date_modified: str
+
+
+#For content modification
+class Content_Modification_Note(BaseModel):
+    content: str
+    date_modified: str
+
 
 #Initialize database connection
 connection = sqlite3.connect(DATABASE_FILE)
 cursor = connection.cursor()
+
 
 #Initialize notes table
 cursor.execute('''CREATE TABLE IF NOT EXISTS Notes(
@@ -32,6 +54,7 @@ Name TEXT PRIMARY KEY NOT NULL,
 Content TEXT NOT NULL,
 "Date Created" TEXT NOT NULL,
 'Date Modified' TEXT NOT NULL)''')
+
 
 #post request to add a note to the database
 @app.post("/note/")
@@ -41,10 +64,12 @@ def add_note(note_name: str, content: str, date_created: str, date_modified: str
     
     return helper.add_single_note(note_name, content, date_created, date_modified)
 
+
 #get request for all notes
 @app.get("/notes")
 def get_all_notes():
     return helper.return_all_notes()
+
 
 #get request for contents of note with unique name
 @app.get("/note/{note_name}")
@@ -54,6 +79,7 @@ def get_note(note_name: str):
     
     return helper.return_note(note_name)
 
+
 #put request to modify a note or create note if it does not exist, update date_modified
 @app.put("/note/{note_name}")
 def modify_note(note_name: str, content: str, date_modified: str):
@@ -62,6 +88,7 @@ def modify_note(note_name: str, content: str, date_modified: str):
     
     return helper.modify_note(content, date_modified, note_name)
 
+
 #put endpoint that modifys a notes name only
 @app.put("/note/{note_name}/rename")
 def change_name(note_name: str, new_name: str):
@@ -69,6 +96,7 @@ def change_name(note_name: str, new_name: str):
         raise HTTPException(status_code = 404, detail = "No note with name exists")
     
     return helper.change_note_name(new_name, note_name)
+
 
 #delete note endpoint
 @app.delete("/note/{note_name}")
